6b2cdeb30355ec0dac418aafef9c6e0e2cbe46ba
Jack <jack@jacks-air.localdomain>
2020-05-02 11:51:42 +0200

    Ensure audio contexts are resumed

--- js/sine_listen.js
+++ js/sine_listen.js
@@ -238,6 +238,7 @@ vis.on("mousemove", mouseMove);
 
 function toggleSound()
 {
+  audioCtx.resume();
   gainNode.gain.value = IsPlayingSound ? 0.00 : 0.05;
   IsPlayingSound = !IsPlayingSound;
   soundToggle.text(IsPlayingSound ? "Mute Sound" : "Play Sound");
